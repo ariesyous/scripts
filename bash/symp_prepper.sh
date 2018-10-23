5d254c34a6494198d53893efd09796d239f553bc
aries-strato <30353494+aries-strato@users.noreply.github.com>
2018-10-23 14:54:37 +0800

    added url

--- bash/symp_prepper.sh
+++ bash/symp_prepper.sh
@@ -5,27 +5,11 @@
 # only works on bright sites and default allocations with default credentials
 # dbs engine version list --inc -f json for list of engines
 # jq '.[] | select(.name == "5.7.00") | .id' for mysql 5.7
+#
+# https://raw.githubusercontent.com/aries-strato/scripts/master/bash/symp_prepper.sh
+#
 
-display_usage() {
-	echo "This script will pull the xenial cloud image, make it public and enable MySQL 5.7 (along with enabling dbaas service)"
-	echo -e "\nUsage:\ncluster-ip\n"
-	}
-
-# if less than 1 arguments supplied, display usage
-if [  $# -lt 1 ]
-then
-	display_usage
-	exit 1
-fi
-
-# check whether user had supplied -h or --help . If yes display usage
-if [[ ( $# == "--help") ||  $# == "-h" ]]
-then
-	display_usage
-	exit 0
-fi
-
-clusterip=$1
+clusterip="https://virtual-nb.service.strato"
 
 echo Pulling Xenial image.
 
