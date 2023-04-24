1c2d8ea3edbf6b9634c69b75b58afe4616e37000
Aries Youssefian <30353494+ariesyous@users.noreply.github.com>
2023-04-24 17:42:01 +0000

    Fixed zone issue

--- gcp/snapshot_schedule.sh
+++ gcp/snapshot_schedule.sh
@@ -12,28 +12,33 @@
 # Replace [YOUR_PROJECT_ID] with your Google Cloud project ID.
 PROJECT_ID=[YOUR_PROJECT_ID]
 
-# Loop through all instances in the specified project.
-for instance in $(gcloud compute instances list --project $PROJECT_ID --format="value(name)")
+# Loop through all instances in the specified project and get their names and zones.
+for instance_info in $(gcloud compute instances list --project $PROJECT_ID --format="csv[no-heading](name,zone)")
 do
+  # Extract the instance name and zone from the retrieved information.
+  IFS=','
+  read -ra INSTANCE_DATA <<< "$instance_info"
+  instance="${INSTANCE_DATA[0]}"
+  ZONE="${INSTANCE_DATA[1]}"
+
   # Get the labels for the current instance.
-  LABELS=$(gcloud compute instances describe $instance --project $PROJECT_ID --format="value(labels)")
+  LABELS=$(gcloud compute instances describe $instance --project $PROJECT_ID --zone $ZONE --format="value(labels)")
+
+  # Get the disk name of the instance.
+  DISK_NAME=$(gcloud compute instances describe $instance --project $PROJECT_ID --zone $ZONE --format="value(disks[0].deviceName)")
 
   # Check if the instance has the "env=prod" label.
   if [[ $LABELS == *"env=prod"* ]]; then
-    # Get the zone of the instance.
-    ZONE=$(gcloud compute instances list --project $PROJECT_ID --filter="name=$instance" --format="value(zone)")
     # Attach the "snapshot-schedule-prod" resource policy to the instance's disks.
-    gcloud compute disks add-resource-policies $instance \
+    gcloud compute disks add-resource-policies $DISK_NAME \
       --project $PROJECT_ID \
       --resource-policies snapshot-schedule-prod \
       --zone $ZONE
 
   # Check if the instance has the "env=nonprod" label.
   elif [[ $LABELS == *"env=nonprod"* ]]; then
-    # Get the zone of the instance.
-    ZONE=$(gcloud compute instances list --project $PROJECT_ID --filter="name=$instance" --format="value(zone)")
     # Attach the "snapshot-schedule-nonprod" resource policy to the instance's disks.
-    gcloud compute disks add-resource-policies $instance \
+    gcloud compute disks add-resource-policies $DISK_NAME \
       --project $PROJECT_ID \
       --resource-policies snapshot-schedule-nonprod \
       --zone $ZONE
